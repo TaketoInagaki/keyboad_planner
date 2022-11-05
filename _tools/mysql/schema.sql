ab72a3878c6e6168c5633a772e288d29c61663b1
TaketoInagaki <genius2taketo@gmail.com>
2022-11-05 15:44:16 +0900

    #1 タスクテーブルを元に戻した

--- _tools/mysql/schema.sql
+++ _tools/mysql/schema.sql
@@ -12,15 +12,14 @@ CREATE TABLE `user`
 
 CREATE TABLE `task`
 (
-    `id`        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'タスクの識別子',
-    `user_id`   BIGINT UNSIGNED NOT NULL COMMENT 'タスクを作成したユーザーの識別子',
+    `id`       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'タスクの識別子',
+    `user_id`  BIGINT UNSIGNED NOT NULL COMMENT 'タスクを作成したユーザーの識別子',
     `title`    VARCHAR(128) NOT NULL COMMENT 'タスクのタイトル',
-    `date`      DATETIME(6) NOT NULL COMMENT 'タスクの日程',
-    `date_type` VARCHAR(20) NOT NULL COMMENT 'タスクの種類',
-    `created`   DATETIME(6) NOT NULL COMMENT 'レコード作成日時',
-    `modified`  DATETIME(6) NOT NULL COMMENT 'レコード修正日時',
+    `status`   VARCHAR(20)  NOT NULL COMMENT 'タスクの状態',
+    `created`  DATETIME(6) NOT NULL COMMENT 'レコード作成日時',
+    `modified` DATETIME(6) NOT NULL COMMENT 'レコード修正日時',
     PRIMARY KEY (`id`),
-    CONSTRAINT `fk_task_user_id`
+    CONSTRAINT `fk_user_id`
         FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
             ON DELETE RESTRICT ON UPDATE RESTRICT
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='タスク';

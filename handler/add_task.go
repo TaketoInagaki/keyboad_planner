b9c7599e816f1e525c6e75380f3cbd160ec27112
TaketoInagaki <genius2taketo@gmail.com>
2022-11-05 15:49:06 +0900

    #1 add_taskを元に戻した

--- handler/add_task.go
+++ handler/add_task.go
@@ -17,8 +17,6 @@ func (at *AddTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	var b struct {
 		Title string `json:"title" validate:"required"`
-		DateType string `json:"date_type" validate:"required"`
-		Date string `json:"date" validate:"required"`
 	}
 	if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
 		RespondJSON(ctx, w, &ErrResponse{

f600dac89fbecaf4de3095819c75e733a0902e2c
TaketoInagaki <genius2taketo@gmail.com>
2022-12-18 14:16:40 +0900

    #22 コードを整形

--- store/reflection.go
+++ store/reflection.go
@@ -43,9 +43,12 @@ func (r *Repository) CreateReflection(
 				WHERE user_id = ?
 					AND date = ?
 					AND week_number = ?;`
-	preDb.SelectContext(ctx, &reflections, preSql, ref.UserID, ref.Date, ref.WeekNumber)
+	err := preDb.SelectContext(ctx, &reflections, preSql, ref.UserID, ref.Date, ref.WeekNumber)
 	if len(reflections) >= 1 {
 		return fmt.Errorf("この日程の振り返りはすでに存在します")
+	}
+	if err != nil {
+		return err
 	}
 	sql := `INSERT INTO reflection(
 		user_id, content, date,
